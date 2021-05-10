d958fbdbf645a287767a79a76fb8adcc0f96d507
Rane Magno <270030927@bun2-206-23.acg.ac.nz>
2021-05-11 11:59:50 +1200

    loooooops

---
+++ Order Form/data.js
@@ -0,0 +1,9 @@
+var promo = {
+  lessThan15: ['1 Fries', '1 Hashbrown Coupon', '1 Free Sauce'],
+  moreThan15: ['1 Burger', '1 Drink', '1 Icecream'],
+  moreThan20: ['1 Burger', '2 Drinks', '2 Icecream', '1 Car Wash Ticket']
+}
+
+// var lessThan15 = ['1 Fries', '1 Hashbrown Coupon', '1 Free Sauce'];
+// var moreThan15 = ['1 Burger', '1 Drink', '1 Icecream'];
+// var moreThan20 = ['1 Burger', '2 Drinks', '2 Icecream', '1 Car Wash Ticket'];
