0917e7bc134018796d20a78e454640da8dd30acf
Rane Magno <270030927@bun2-206-23.acg.ac.nz>
2021-05-17 12:22:27 +1200

    holiday task

--- Wk3/holiday/js/custom.js
+++ Wk3/holiday/js/custom.js
@@ -1,111 +1,82 @@
-// Plan of attack
+// Task A Psuedo
 // 1. Label classes and grab data, make variables
 // 2. Create for loops to put data in DOM
 // 3. Calculate total price
 
+// Task B Psuedo
+// 1. Print total on DOM by altering element text content
+// 2. Make panel on right side where total will be shown
+// 3. Gather 4 images and place them into the right panel
+// 4. Turn images into selections
+// 5. Only display selected images after book
+// 6. Add animations or style changes to show that you have selected the image
 
-var holidayPlaces = document.querySelectorAll('#holiday option');
-var hotelNames = document.querySelectorAll('#hotel option');
-var bookBtn = document.querySelector('#bookBtn');
+(function(){
 
-bookBtn.addEventListener('click', calculatePrice, false)
+  var holidayPlaces = document.querySelectorAll('#holiday option');
+  var hotelNames = document.querySelectorAll('#hotel option');
+  var bookBtn = document.querySelector('#bookBtn');
+  var holidayData = Object.entries(data);
+  var placeAndName = Object.values(data);
 
-var holidayData = Object.entries(data);
+  var holidayImg = document.querySelector('#holidayImg');
+  var hotelImg= document.querySelector('#hotelImg');
 
-// console.log(holidayData[0]);
-// console.log(holidayData[0][0]);
-
-
-var placeAndName = Object.values(data);
+  bookBtn.addEventListener('click', calculatePrice, false)
 
 
-// Loop to show data
-for (var i = 0; i < holidayData.length; i++) {
-  // console.log(holidayData[i]);
-  // console.log(holidayData[i][i]); accesses the array within array
+  // START - Loop to show data
+  for (var i = 0; i < holidayData.length; i++) {
 
-  if (holidayData[i][0] === "holidayOne"){
-    holidayPlaces[i].textContent = placeAndName[i].place;
-  } else if (holidayData[i][0] === "holidayTwo"){
-    holidayPlaces[i].textContent = placeAndName[i].place;
-  } else if (holidayData[i][0] === "hotelOne") {
-    hotelNames[0].textContent = placeAndName[i].name;
-  } else {
-    hotelNames[1].textContent = placeAndName[i].name;
+    // console.log(holidayData[i]);
+    // console.log(holidayData[i][i]); accesses the array within array
+
+
+    if (holidayData[i][0] === "holidayOne"){
+      holidayPlaces[i].textContent = placeAndName[i].place;
+    } else if (holidayData[i][0] === "holidayTwo"){
+      holidayPlaces[i].textContent = placeAndName[i].place;
+    } else if (holidayData[i][0] === "hotelOne") {
+      hotelNames[0].textContent = placeAndName[i].name;
+    } else {
+      hotelNames[1].textContent = placeAndName[i].name;
+    }
   }
-}
+  // END - Loop to show data
 
 
-var holSelect = document.querySelector('#holiday')
+  // START - Calculate and show total price and img
+  function calculatePrice() {
+    var holSelect = document.querySelector('#holiday')
+    var holSelected = holSelect.value;
+    var hotSelect = document.querySelector('#hotel')
+    var hotSelected = hotSelect.value;
+    var holidayPrice = 0;
+    var hotelPrice = 0;
+  
+    if (holSelected === 'Samoa') {
+      holidayPrice = holidayPrice + placeAndName[0].price;
+      holidayImg.style.backgroundImage = placeAndName[0].img;
+    }
+    if (holSelected === 'Fiji'){
+      holidayPrice = holidayPrice + placeAndName[1].price;
+      holidayImg.style.backgroundImage = placeAndName[1].img;
+    }
+    if (hotSelected === 'Hilton'){
+      hotelPrice = hotelPrice + placeAndName[2].pricePerNight;
+      hotelImg.style.backgroundImage = placeAndName[2].img;
+    }
+    if (hotSelected === 'Sheraton'){
+      hotelPrice = hotelPrice + placeAndName[3].pricePerNight;
+      hotelImg.style.backgroundImage = placeAndName[3].img;
+    }
+    var totalPrice = holidayPrice + hotelPrice;
+    console.log('Total price - $' + totalPrice);
 
-// Calculate total price
-function calculatePrice(holidayPrice) {
-  var holidayPrice = 0;
-  console.dir(holSelect);
-  console.log(holSelect[0]);
-  console.log(holidayPlaces[0].textContent);
-  console.log(hotelNames);
-
-  if (holidayPlaces.textContent) {
-
+    var totalDisplay = document.querySelector('#totalDisplay')
+    totalDisplay.textContent = 'Total: $' + totalPrice;
   }
+  // END - Calculate and show total price
 
-
-  for (var i = 0; i < holSelect.length; i++) {
-    console.log(holSelect[i].textContent);
-  }
-  for (var i = 0; i < holidayPlaces.length; i++) {
-    console.log(holidayPlaces[i].textContent);
-  }
-
-  // for (var i = 0; i < holidayPlaces.length; i++) {
-  //   console.log(holidayPlaces[i]);
-  //   if (holidayPlaces[i] === 'Samoa') {
-  //     holidayPrice = holidayPrice + placeAndName[0].price;
-  //     break;
-  //   } else {
-  //     holidayPrice = holidayPrice + placeAndName[1].price;
-  //   }
-  // }
-    console.log('Total price - $' + holidayPrice);
-}
-
-//
-//   console.dir(holidayPlaces[0].textContent);
-//   if (holidayPlaces[0].textContent === 'Samoa') {
-//     holidayPrice = holidayPrice + placeAndName[0].price;
-//   }
-//    if (holidayPlaces[1].text === 'Fiji') {
-//     holidayPrice = holidayPrice + placeAndName[1].price;
-//     console.log('hello');
-//   }
-//    if (hotelNames[0].text === 'Hilton') {
-//     holidayPrice = holidayPrice + placeAndName[2].price;
-//     console.log('wow');
-//   }
-//   if (hotelNames[1].text === 'Sheraton') {
-//     holidayPrice = holidayPrice + placeAndName[3].price;
-//     console.log('gday');
-//   }
-//   console.log('Total price - $' + holidayPrice);
-// }
-
-
-
-  // displays the holidays and hotels
-//   for (var i = 0; i < holidayPlaces.length; i++) {
-//     holidayPlaces[i].textContent = holidayData[i].place;
-//   }
-//   for (var i = 0; i < hotelNames.length; i++) {
-//     hotelNames[i].textContent = holidayData[i + 2].name;
-//   }
-//
-// // console.log(hotelNames.length);
-//   // calculates price
-//   function calculatePrice() {
-//     console.log("here's a price");
-//     console.log(Object.entries(hotelNames));
-//     if (hotelNames.textContent === 'Hilton'){
-//       console.log('this is expensive');
-//     }
-//   }
+}())
+// END - IIFE
--- Wk3/holiday/js/holiday-data.js
+++ Wk3/holiday/js/holiday-data.js
@@ -1,18 +1,22 @@
 var data = {
   holidayOne: {
     place: 'Samoa',
-    price: 889
+    price: 889,
+    img: 'url("imgs/holiday1.jpg")'
   },
   holidayTwo: {
     place: 'Fiji',
-    price: 759
+    price: 759,
+    img: 'url("imgs/holiday2.jpg")'
   },
   hotelOne: {
     name: 'Hilton',
-    pricePerNight: 269
+    pricePerNight: 269,
+    img: 'url("imgs/hotel1.jpg")'
   },
   hotelTwo: {
     name: 'Sheraton',
-    pricePerNight: 249
+    pricePerNight: 249,
+    img: 'url("imgs/hotel2.jpg")'
   }
 }
