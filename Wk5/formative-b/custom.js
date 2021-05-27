25cfac49b87ac41bbbe822ea0a720c4869ac747d
Rane Magno <270030927@bun2-206-23.acg.ac.nz>
2021-05-28 09:16:45 +1200

    formative b

--- Wk5/formative-b/custom.js
+++ Wk5/formative-b/custom.js
@@ -7,7 +7,7 @@
     var imgResult = document.querySelector('#image').value;
     var bgResult = document.querySelector('#background').value;
     var fontResult = document.querySelector('#font').value;
-    var msgResult = document.querySelector('#message');
+    var msgResult = document.querySelector('#message').value;
 
     // Change card image
     var bgImg = document.querySelector('#bgImg');
@@ -36,55 +36,58 @@
     }
 
     // Change font
-    var font = document.querySelector('#cardFont');
+    var text = document.querySelector('#cardText');
+    text.textContent = msgResult
     if (fontResult === 'handwriting'){
-      font.classList.add('handwriting');
-      font.classList.remove('sketch');
-      font.classList.remove('print');
+      text.classList.add('handwriting');
+      text.classList.remove('sketch');
+      text.classList.remove('print');
     } else if (fontResult === 'print'){
-      console.log('print font');
-      font.classList.add('print');
-      font.classList.remove('sketch');
-      font.classList.remove('handwriting');
+      text.classList.add('print');
+      text.classList.remove('sketch');
+      text.classList.remove('handwriting');
     } else {
-      console.log('sketch font');
-      font.classList.add('sketch');
-      font.classList.remove('handwriting');
-      font.classList.remove('print');
+      text.classList.add('sketch');
+      text.classList.remove('handwriting');
+      text.classList.remove('print');
+    }
+    // Change Border ------ issue -------
+    var borderResult = document.querySelector('#border').value;
+    var border = document.querySelector('#cardBorder');
+    if (borderResult === 'thin'){
+      console.log('thin');
+      border.classList.add('smallBorder');
+      border.classList.remove('bigBorder');
+      border.classList.remove('filledBorder');
+      border.classList.remove('noneBorder');
+    }
+    if (borderResult === 'thin'){
+      border.classList.add('smallBorder');
+      border.classList.remove('bigBorder');
+      border.classList.remove('filledBorder');
+      border.classList.remove('noneBorder');
+      console.log('thin');
+    } else if (borderResult === 'thick'){
+      border.classList.add('bigBorder');
+      border.classList.remove('smallBorder');
+      border.classList.remove('filledBorder');
+      border.classList.remove('noneBorder');
+      console.log('thick');
+    } else if (borderResult === 'filled'){
+      border.classList.add('filledBorder');
+      border.classList.remove('smallBorder');
+      border.classList.remove('bigBorder');
+      border.classList.remove('noneBorder');
+      console.log('filled');
+    } else {
+      border.classList.add('noneBorder');
+      border.classList.remove('smallBorder');
+      border.classList.remove('bigBorder');
+      border.classList.remove('filledBorder');
+      console.log('none');
     }
   }
 
-  // Change Border
-  var borderResult = document.querySelector('#border').value;
-  var border = document.querySelector('#cardBorder');
-  if (borderResult === 'thin'){
-    console.log('thin');
-  }
-  // if (borderResult === 'thin'){
-  //   border.classList.add('smallBorder:after');
-  //   border.classList.remove('bigBorder:after');
-  //   border.classList.remove('filledBorder:after');
-  //   border.classList.remove('noneBorder:after');
-  //   console.log('thin');
-  // } else if (borderResult === 'thick'){
-  //   border.classList.add('bigBorder:after');
-  //   border.classList.remove('smallBorder:after');
-  //   border.classList.remove('filledBorder:after');
-  //   border.classList.remove('noneBorder:after');
-  //   console.log('thick');
-  // } else if (borderResult === 'filled'){
-  //   border.classList.add('filledBorder:after');
-  //   border.classList.remove('smallBorder:after');
-  //   border.classList.remove('bigBorder:after');
-  //   border.classList.remove('noneBorder:after');
-  //   console.log('filled');
-  // } else {
-  //   border.classList.add('noneBorder');
-  //   border.classList.remove('smallBorder:after');
-  //   border.classList.remove('bigBorder:after');
-  //   border.classList.remove('filledBorder:after');
-  //   console.log('none');
-  //
-  // }
+
 
 }());
